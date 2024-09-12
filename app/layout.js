5f6f3e8de8c07f9046ddc8584463a0d3bf20eb89
Subrata Guchhait <sguchhaitdev@gmail.com>
2024-09-12 23:24:49 +0530

    layout && assets

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,34 @@
+/* eslint-disable camelcase */
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata = {
+  title: "CourseGen",
+  description:
+    "A learning platform where we can create your own course and see other cxourses",
+  icons: {
+    icon: "coursegen.ico", // Use the correct path relative to the 'public' folder
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <main>{children}</main>
         </body>
       </html>
